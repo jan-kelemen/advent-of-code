e5f5e25ee92a772601fd09d512580fa5c9983b23
Jan Kelemen <jkeleme@gmail.com>
2016-12-04 23:41:46 +0100

    add day 03 part 2 solution

--- 2016/d03/d03.cpp
+++ 2016/d03/d03.cpp
@@ -8,16 +8,18 @@ using triangle_sides_t = std::tuple<int, int, int>;
 
 std::vector<triangle_sides_t> read_triangles(char const* filename);
 
+int count_valid_triangles(std::vector<triangle_sides_t> const& triangles);
+
+void transform_input(std::vector<triangle_sides_t>& triangles);
+
 int main() {
     auto constexpr input_filename = "input.txt";
     auto triangles = read_triangles(input_filename);
-    auto valid_triangles_count = std::count_if(triangles.begin(), triangles.end(), [](auto& t) {
-        return (std::get<0>(t) + std::get<1>(t) > std::get<2>(t)) &&
-            (std::get<1>(t) + std::get<2>(t) > std::get<0>(t)) &&
-            (std::get<0>(t) + std::get<2>(t) > std::get<1>(t));
-    });
+    auto horizontal_triangles = count_valid_triangles(triangles);
+    transform_input(triangles);
+    auto vertical_triangles = count_valid_triangles(triangles);
 
-    std::cout << valid_triangles_count << '\n';
+    std::cout << horizontal_triangles << ", " << vertical_triangles << '\n';
     system("pause");
 }
 
@@ -32,4 +34,20 @@ std::vector<triangle_sides_t> read_triangles(char const* filename) {
     }
 
     return rv;
+}
+
+int count_valid_triangles(std::vector<triangle_sides_t> const& triangles) {
+    return std::count_if(triangles.begin(), triangles.end(), [](auto& t) {
+        return (std::get<0>(t) + std::get<1>(t) > std::get<2>(t)) &&
+            (std::get<1>(t) + std::get<2>(t) > std::get<0>(t)) &&
+            (std::get<0>(t) + std::get<2>(t) > std::get<1>(t));
+    });
+}
+
+void transform_input(std::vector<triangle_sides_t>& triangles) {
+    for (size_t i = 0; i < triangles.size() - 2; i += 3) {
+        std::swap(std::get<0>(triangles[i + 1]), std::get<1>(triangles[i]));
+        std::swap(std::get<0>(triangles[i + 2]), std::get<2>(triangles[i]));
+        std::swap(std::get<1>(triangles[i + 2]), std::get<2>(triangles[i + 1]));
+    }
 }
