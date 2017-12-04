5c1610285dc986c7936d5a74651aa4104b3ba25e
Jan Kelemen <jkeleme@gmail.com>
2017-12-04 18:43:40 +0100

    solution for day 04

---
+++ 2017/D04/D04-1/main.cpp
@@ -0,0 +1,65 @@
+#include <algorithm>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <unordered_set>
+#include <vector>
+
+using passphrase_t = std::vector<std::string>;
+
+std::vector<passphrase_t> read_passphrases(std::istream& stream);
+
+bool is_valid_passhprase(passphrase_t const& passphrase);
+
+int count_valid_passphrases(std::vector<passphrase_t> const& passphrases);
+
+int main(int argc, char** argv)
+{
+    auto passphrases = read_passphrases(std::cin);
+    auto valid = count_valid_passphrases(passphrases);
+    std::cout << valid << '\n';
+}
+
+std::vector<std::vector<std::string>> read_passphrases(std::istream& stream)
+{
+    auto rv = std::vector<std::vector<std::string>>();
+
+    std::string line;
+    while(std::getline(stream, line))
+    {
+        auto passphrase = passphrase_t();
+
+        auto word_stream = std::istringstream(line);
+
+        std::string word;
+        while(word_stream >> word)
+        {
+            passphrase.push_back(word);
+        }
+
+        rv.emplace_back(std::move(passphrase));
+    }
+
+    return rv;
+}
+
+bool is_valid_passhprase(passphrase_t const& passphrase)
+{
+    auto set = std::unordered_set<std::string>();
+    for(auto const& word : passphrase)
+    {
+        if(set.count(word))
+        {
+            return false;
+        }
+        set.insert(word);
+    }
+    return true;
+}
+
+int count_valid_passphrases(std::vector<passphrase_t> const& passphrases)
+{
+    return std::count_if(passphrases.begin(), passphrases.end(), [](passphrase_t const& p) {
+        return is_valid_passhprase(p);
+    });
+}
--- 2017/README.md
+++ 2017/README.md
@@ -2,3 +2,4 @@
 * Day 01: Inverse Captcha - 1029, 1220
 * Day 02: Corruption Checksum - 45351, 275
 * Day 03: Spiral Memory - 430, 312453
+* Day 04: High-Entropy Passphrases - 455, 186
