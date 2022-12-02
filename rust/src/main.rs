bd7bf34ffdee0cbb5233487fc860dfdfb42d0448
Jan Kelemen <jkeleme@gmail.com>
2022-12-02 22:27:22 +0100

    Solution for day 02

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,16 +1,23 @@
+use aoclib;
 use std::env;
 use std::fs::File;
-use aoclib;
 
 fn main() {
-    let args : Vec<String> = env::args().collect();
-    let (p1, p2) = match args[1].parse<u8>() {
+    let args: Vec<String> = env::args().collect();
+    let (p1, p2) = match args[1].parse::<u8>() {
         Ok(n) => match n {
-            1 => (aoclib::d01p01(File::open("res/d01.txt").unwrap()), aoclib::d01p02(File::open("res/d01.txt").unwrap())),
+            1 => (
+                aoclib::d01p01(File::open("res/2015/d01.txt").unwrap()),
+                aoclib::d01p02(File::open("res/2015/d01.txt").unwrap()),
+            ),
+            2 => (
+                aoclib::d02p01(File::open("res/2015/d02.txt").unwrap()),
+                aoclib::d02p02(File::open("res/2015/d02.txt").unwrap()),
+            ),
             e => panic!("Unkown day: {}", e),
-        }
+        },
         Err(e) => panic!("Unknown day: {}", e),
-    }
+    };
 
     print!("{} {}", p1, p2);
 }
