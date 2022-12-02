74165ae74e156319ebce28e6127ba68082c603c9
Jan Kelemen <jkeleme@gmail.com>
2022-12-02 16:37:52 +0100

    Solutions for day 02 & init other days

--- 2022/README.md
+++ 2022/README.md
@@ -1,3 +1,3 @@
 ## Solutions 
 * Day 01: Calorie Counting - 72240, 210957
-
+* Day 02: Rock Paper Scissors - 10404, 10334
---
+++ 2022/d21/p01/main.cpp
@@ -0,0 +1,9 @@
+#include <fstream>
+#include <iostream>
+
+int main([[maybe_unused]] int argc, char** argv)
+{
+    std::ifstream input {argv[1]};
+
+    std::cout << '\n';
+}
