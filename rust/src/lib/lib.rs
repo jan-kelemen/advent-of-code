52ad155f76d7ff9c192b13f7b52a4ca7a1fd7ee6
Jan Kelemen <jkeleme@gmail.com>
2022-12-03 20:28:54 +0100

    Solution for day 04

--- rust/src/lib/lib.rs
+++ rust/src/lib/lib.rs
@@ -1,3 +1,4 @@
 pub mod i_was_told_there_would_be_no_math;
 pub mod not_quite_lisp;
 pub mod perfectly_spherical_houses_in_a_vacuum;
+pub mod the_ideal_stocking_stuffer;
--- rust/src/main.rs
+++ rust/src/main.rs
@@ -2,26 +2,33 @@ use aoclib::*;
 use std::env;
 use std::fs::File;
 
+fn input_file(day: u8) -> File {
+    File::open(format!("res/2015/d{:02}.txt", day)).unwrap()
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
     let (p1, p2) = match args[1].parse::<u8>() {
         Ok(n) => match n {
             1 => (
-                not_quite_lisp::part1(File::open("res/2015/d01.txt").unwrap()),
-                not_quite_lisp::part2(File::open("res/2015/d01.txt").unwrap()),
+                not_quite_lisp::part1(input_file(n)),
+                not_quite_lisp::part2(input_file(n)),
             ),
             2 => (
-                i_was_told_there_would_be_no_math::part1(File::open("res/2015/d02.txt").unwrap()),
-                i_was_told_there_would_be_no_math::part2(File::open("res/2015/d02.txt").unwrap()),
+                i_was_told_there_would_be_no_math::part1(input_file(n)),
+                i_was_told_there_would_be_no_math::part2(input_file(n)),
             ),
             3 => (
                 perfectly_spherical_houses_in_a_vacuum::part1(
-                    File::open("res/2015/d03.txt").unwrap(),
+                    input_file(n)
                 ),
                 perfectly_spherical_houses_in_a_vacuum::part2(
-                    File::open("res/2015/d03.txt").unwrap(),
+                    input_file(n)
                 ),
             ),
+            4 => (
+                the_ideal_stocking_stuffer::part1(input_file(n)),
+                the_ideal_stocking_stuffer::part2(input_file(n))),
             e => panic!("Unkown day: {}", e),
         },
         Err(e) => panic!("Unknown day: {}", e),
