97f12ff03c390f38c1868fdc1d826836f477801f
Jan Kelemen <jkeleme@gmail.com>
2022-12-03 14:33:29 +0100

    Organize solutions into modules

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,4 +1,4 @@
-use aoclib;
+use aoclib::*;
 use std::env;
 use std::fs::File;
 
@@ -7,12 +7,16 @@ fn main() {
     let (p1, p2) = match args[1].parse::<u8>() {
         Ok(n) => match n {
             1 => (
-                aoclib::d01p01(File::open("res/2015/d01.txt").unwrap()),
-                aoclib::d01p02(File::open("res/2015/d01.txt").unwrap()),
+                not_quite_lisp::final_floor(File::open("res/2015/d01.txt").unwrap()),
+                not_quite_lisp::basement_entered(File::open("res/2015/d01.txt").unwrap()),
             ),
             2 => (
-                aoclib::d02p01(File::open("res/2015/d02.txt").unwrap()),
-                aoclib::d02p02(File::open("res/2015/d02.txt").unwrap()),
+                i_was_told_there_would_be_no_math::needed_wrapping_paper_area(
+                    File::open("res/2015/d02.txt").unwrap(),
+                ),
+                i_was_told_there_would_be_no_math::needed_feet_of_ribbon(
+                    File::open("res/2015/d02.txt").unwrap(),
+                ),
             ),
             e => panic!("Unkown day: {}", e),
         },
