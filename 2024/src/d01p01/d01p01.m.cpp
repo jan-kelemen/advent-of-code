76d3ea5f371be4037b8ee9d563ed0c2a0737a8b9
Jan Kelemen <jkeleme@gmail.com>
2024-12-01 12:57:44 +0100

    Solution for day 01

--- 2024/src/d01p01/d01p01.m.cpp
+++ 2024/src/d01p01/d01p01.m.cpp
@@ -1,1 +1,38 @@
-int main() { }
+#include <algorithm>
+#include <cassert>
+#include <cstdint>
+#include <fstream>
+#include <iostream>
+#include <utility>
+#include <vector>
+
+int main([[maybe_unused]] int const argc, char const* argv[])
+{
+    assert(argc >= 2);
+    std::ifstream input{argv[1]};
+
+    std::vector<uint64_t> left_list;
+    std::vector<uint64_t> right_list;
+
+    uint64_t left, right;
+    while (input >> left >> right)
+    {
+        left_list.push_back(left);
+        right_list.push_back(right);
+    }
+    assert(left_list.size() == right_list.size());
+
+    std::ranges::sort(left_list);
+    std::ranges::sort(right_list);
+
+    uint64_t distance{};
+    for (size_t i{}; i != left_list.size(); ++i)
+    {
+        auto const l{left_list[i]};
+        auto const r{right_list[i]};
+
+        distance += std::cmp_less(l, r) ? r - l : l - r;
+    }
+
+    std::cout << distance << '\n';
+}
--- 2024/src/d01p02/d01p02.m.cpp
+++ 2024/src/d01p02/d01p02.m.cpp
@@ -1,1 +1,32 @@
-int main() { }
+#include <cassert>
+#include <cstdint>
+#include <fstream>
+#include <iostream>
+#include <map>
+#include <vector>
+
+int main([[maybe_unused]] int const argc, char const* argv[])
+{
+    assert(argc >= 2);
+    std::ifstream input{argv[1]};
+
+    std::vector<uint64_t> left_list;
+    std::map<uint64_t, size_t> right_list;
+
+    uint64_t left, right;
+    while (input >> left >> right)
+    {
+        left_list.push_back(left);
+        ++right_list[right];
+    }
+
+    uint64_t similarity{};
+    for (uint64_t const l : left_list)
+    {
+        auto const r{right_list[l]};
+
+        similarity += l * r;
+    }
+
+    std::cout << similarity << '\n';
+}
