c38843e28fa42c52583d83f85c6be78fd8ca94c5
Jan Kelemen <jkeleme@gmail.com>
2025-12-11 21:11:20 +0100

    Add solution for day 19 part 2

--- rust/src/lib/medicine_for_rudolph.rs
+++ rust/src/lib/medicine_for_rudolph.rs
@@ -55,27 +55,90 @@ fn read_replacements(input: File) -> (String, HashMap<String, Vec<String>>) {
     (molecule, replacements)
 }
 
+#[derive(PartialEq)]
+enum Symbol {
+    Al,
+    Ar,
+    B,
+    C,
+    Ca,
+    e,
+    F,
+    H,
+    Mg,
+    N,
+    O,
+    P,
+    Rn,
+    Si,
+    Th,
+    Ti,
+    Y,
+}
+
+fn convert_medicine_to_symbols(medicine: &String) -> Vec<Symbol> {
+    let mut rv = Vec::new();
+
+    for (i, str) in medicine.match_indices(|c: char| c.is_uppercase() || c == 'e') {
+        match str {
+            "A" => match medicine.chars().nth(i + 1) {
+                Some('l') => rv.push(Symbol::Al),
+                Some('r') => rv.push(Symbol::Ar),
+                _ => panic!("Unrecognized symbol A"),
+            },
+            "B" => rv.push(Symbol::B),
+            "C" => match medicine.chars().nth(i + 1) {
+                Some('a') => rv.push(Symbol::Ca),
+                _ => rv.push(Symbol::C),
+            },
+            "e" => rv.push(Symbol::e),
+            "F" => rv.push(Symbol::F),
+            "H" => rv.push(Symbol::H),
+            "M" => rv.push(Symbol::Mg),
+            "N" => rv.push(Symbol::N),
+            "O" => rv.push(Symbol::O),
+            "P" => rv.push(Symbol::P),
+            "R" => rv.push(Symbol::Rn),
+            "S" => rv.push(Symbol::Si),
+            "T" => match medicine.chars().nth(i + 1) {
+                Some('h') => rv.push(Symbol::Th),
+                Some('i') => rv.push(Symbol::Ti),
+                _ => panic!("Unrecognized symbol T"),
+            },
+            "Y" => rv.push(Symbol::Y),
+            _ => panic!("Unrecognized symbol {}", str),
+        }
+    }
+
+    rv
+}
+
 pub fn part1(input: File) -> String {
     let (medicine, replacements) = read_replacements(input);
-
     let unique = apply_substitutions(medicine.as_str(), &replacements);
 
     unique.len().to_string()
 }
 
 pub fn part2(input: File) -> String {
-    //    let (medicine, replacements) = read_replacements(input);
+    // If the grammar was in Chomsky Normal Form (CNF) the answer would be 2 * len(medicine) - 1.
+    //
+    // It should be solvable via CYK algorithm, apparently it works even if the input isn't in CNF.
+    // https://en.wikipedia.org/wiki/CYK_algorithm
+    // https://stackoverflow.com/questions/36901948/does-cky-really-require-cnf
 
-    //    let steps = 0;
-    //    let mut molecules: HashSet<String> = HashSet::from(["e".to_string()]);
-    //    while !molecules.contains(&medicine) {
-    //        let mut next : HashSet<String> = HashSet::new();
-    //        for molecule in molecules.iter() {
-    //            next.extend(recurse(molecule, &replacements));
-    //        }
-    //        steps += 1;
-    //        molecules = next;
-    //    }
+    let (medicine, _) = read_replacements(input);
+    let converted = convert_medicine_to_symbols(&medicine);
 
-    "".to_string()
+    // https://www.reddit.com/r/adventofcode/comments/3xflz8/comment/cy4etju/
+    // https://www.reddit.com/r/adventofcode/comments/3xflz8/comment/cy4h7ji/
+    let steps = converted.len()
+        - converted
+            .iter()
+            .filter(|s| **s == Symbol::Ar || **s == Symbol::Rn)
+            .count()
+        - converted.iter().filter(|s| **s == Symbol::Y).count() * 2
+        - 1;
+
+    steps.to_string()
 }
